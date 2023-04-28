4419755faa81b0cda6ef2cd4b45cd4c1a80e4d19
Benjamin Knecht <benjamin.knecht@srf.ch>
2023-04-28 22:43:57 +0200

    wrap in async ..

--- index.js
+++ index.js
@@ -5,80 +5,83 @@ const createWriteStream= require('fs');
 const walkSync = require('walk-sync');
 const {createReadStream} = require("fs");
 
-try {
-    const token = core.getInput('token')
-    const channels = core.getInput('channels')
-    const workdir = 'cypress'
-    const slack = new slackClient.WebClient(token);
+async function run() {
+    try {
+        const token = core.getInput('token')
+        const channels = core.getInput('channels')
+        const workdir = 'cypress'
+        const slack = new slackClient.WebClient(token);
 
-    let message = `${github.context.payload.ref}`;
-    message = message.slice(11);
+        let message = `${github.context.payload.ref}`;
+        message = message.slice(11);
 
-    const videos = walkSync(workdir, { globs: ['**/*.mp4'] })
-    const screenshots = walkSync(workdir, { globs: ['**/*.png'] })
+        const videos = walkSync(workdir, { globs: ['**/*.mp4'] })
+        const screenshots = walkSync(workdir, { globs: ['**/*.png'] })
 
-    if (videos.length <= 0 && screenshots.length <= 0) {
-        core.debug('No videos or screenshots found. Exiting!')
-        core.setOutput('result', 'No videos or screenshots found!')
-        return
-    }
+        if (videos.length <= 0 && screenshots.length <= 0) {
+            core.debug('No videos or screenshots found. Exiting!')
+            core.setOutput('result', 'No videos or screenshots found!')
+            return
+        }
 
-    core.debug('Sending initial slack message')
-    const result = await slack.chat.postMessage({
-        text: 'hold on ...',
-        channel: channels
-    })
+        core.debug('Sending initial slack message')
+        const result = await slack.chat.postMessage({
+            text: 'hold on ...',
+            channel: channels
+        })
 
-    const threadID = result.ts
-    const channelId = result.channel
+        const threadID = result.ts
+        const channelId = result.channel
 
-    if (screenshots.length > 0) {
-        core.debug('Uploading screenshots...')
+        if (screenshots.length > 0) {
+            core.debug('Uploading screenshots...')
 
-        await Promise.all(
-            screenshots.map(async screenshot => {
-                core.debug(`Uploading ${screenshot}`)
+            await Promise.all(
+                screenshots.map(async screenshot => {
+                    core.debug(`Uploading ${screenshot}`)
 
-                await slack.files.upload({
-                    filename: screenshot,
-                    file: createReadStream(`${workdir}/${screenshot}`),
-                    thread_ts: threadID,
-                    channels: channelId
+                    await slack.files.upload({
+                        filename: screenshot,
+                        file: createReadStream(`${workdir}/${screenshot}`),
+                        thread_ts: threadID,
+                        channels: channelId
+                    })
                 })
-            })
-        )
+            )
 
-        core.debug('...done!')
-    }
+            core.debug('...done!')
+        }
 
-    if (videos.length > 0) {
-        core.debug('Uploading videos...')
+        if (videos.length > 0) {
+            core.debug('Uploading videos...')
 
-        await Promise.all(
-            videos.map(async video => {
-                core.debug(`Uploading ${video}`)
+            await Promise.all(
+                videos.map(async video => {
+                    core.debug(`Uploading ${video}`)
 
-                await slack.files.upload({
-                    filename: video,
-                    file: createReadStream(`${workdir}/${video}`),
-                    thread_ts: threadID,
-                    channels: channelId
+                    await slack.files.upload({
+                        filename: video,
+                        file: createReadStream(`${workdir}/${video}`),
+                        thread_ts: threadID,
+                        channels: channelId
+                    })
                 })
-            })
-        )
+            )
 
-        core.debug('...done!')
+            core.debug('...done!')
+        }
+
+
+        await slack.chat.update({
+            ts: threadID,
+            channel: channels,
+            text: `
+    PR: *${message}*\n
+    From: ${github.context.payload.pusher.name}\n
+            `
+        })
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-
-    await slack.chat.update({
-        ts: threadID,
-        channel: channels,
-        text: `
-PR: *${message}*\n
-From: ${github.context.payload.pusher.name}\n
-        `
-    })
-} catch (error) {
-    core.setFailed(error.message);
 }
+run();
