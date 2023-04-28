19f042d06ad8e9e59dd9ba60d054268acfdee400
Benjamin Knecht <benjamin.knecht@srf.ch>
2023-04-28 22:32:13 +0200

    updated action ..

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const slackClient = require('@slack/web-api');
+const createWriteStream= require('fs');
+const walkSync = require('walk-sync');
+const {createReadStream} = require("fs");
 
 try {
     const token = core.getInput('token')
     const channels = core.getInput('channels')
-    const number = core.getInput('number')
-    console.log("number: "+number);
+    const workdir = 'cypress'
+    const slack = new slackClient.WebClient(token);
 
     let message = `${github.context.payload.ref}`;
     message = message.slice(11);
 
-    const slack = new slackClient.WebClient(token);
-    slack.chat.postMessage({
+    const videos = walkSync(workdir, { globs: ['**/*.mp4'] })
+    const screenshots = walkSync(workdir, { globs: ['**/*.png'] })
+
+    if (videos.length <= 0 && screenshots.length <= 0) {
+        core.debug('No videos or screenshots found. Exiting!')
+        core.setOutput('result', 'No videos or screenshots found!')
+        return
+    }
+
+    core.debug('Sending initial slack message')
+    const result = await slack.chat.postMessage({
+        text: 'hold on ...',
+        channel: channels
+    })
+
+    const threadID = result.ts
+    const channelId = result.channel
+
+    if (screenshots.length > 0) {
+        core.debug('Uploading screenshots...')
+
+        await Promise.all(
+            screenshots.map(async screenshot => {
+                core.debug(`Uploading ${screenshot}`)
+
+                await slack.files.upload({
+                    filename: screenshot,
+                    file: createReadStream(`${workdir}/${screenshot}`),
+                    thread_ts: threadID,
+                    channels: channelId
+                })
+            })
+        )
+
+        core.debug('...done!')
+    }
+
+    if (videos.length > 0) {
+        core.debug('Uploading videos...')
+
+        await Promise.all(
+            videos.map(async video => {
+                core.debug(`Uploading ${video}`)
+
+                await slack.files.upload({
+                    filename: video,
+                    file: createReadStream(`${workdir}/${video}`),
+                    thread_ts: threadID,
+                    channels: channelId
+                })
+            })
+        )
+
+        core.debug('...done!')
+    }
+
+
+    await slack.chat.update({
+        ts: threadID,
         channel: channels,
         text: `
 PR: *${message}*\n
 From: ${github.context.payload.pusher.name}\n
-URL: https://github.com/mmz-srf/nora/pull/${number}
         `
     })
 } catch (error) {
